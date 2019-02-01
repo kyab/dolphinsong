30798da28e7abd5c325a690455597ee4b0043412
kyab <kyossi212@gmail.com>
2019-02-01 23:36:15 +0900

    Add sample file uploader

--- app.rb
+++ app.rb
@@ -4,6 +4,14 @@ require "sinatra/reloader"
 class DolphinSong < Sinatra::Base
 
 	set :bind, "0.0.0.0"
+	configure :development do
+		register Sinatra::Reloader
+	end
+
+	configure :production do
+		register Sinatra::Reloader
+	end
+
 	get "/hello" do
 		"hello"
 	end
@@ -17,6 +25,21 @@ class DolphinSong < Sinatra::Base
 		@foo = "hoge"
 
 		erb :index, :locals=> {:one=>"one", :two=>"two"}
+	end
+
+	post "/upload" do
+
+		@filename = params[:upfile][:filename]
+		save_path = "./public/sounds/#{@filename}"
+		
+		puts "Saving : #{save_path}"
+		File.open(save_path , "wb") do |f|
+			f.write params[:upfile][:tempfile].read
+		end
+		@files = Dir.glob("*" , base:"public/sounds")
+		puts "files = :" 
+		p @files
+		erb :files
 	end
 
 	if app_file == $0
